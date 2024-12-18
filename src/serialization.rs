b9f5e4794948c3ca14133e823d44914dfa02b58b
Safak Turkeli <sfktrkl@gmail.com>
2024-12-18 03:51:01 +0300

    Implement simple serializer

--- src/serialization.rs
+++ src/serialization.rs
@@ -46,3 +46,49 @@ impl Serializer for JsonSerializer {
         serde_json::from_slice(bytes).map_err(|e| e.to_string())
     }
 }
+
+pub struct SimpleSerializer;
+
+impl Serializer for SimpleSerializer {
+    #[cfg(test)]
+    fn serialize_request(&self, request: &Request) -> Vec<u8> {
+        format!(
+            "{} {} {}",
+            request.command,
+            request.key.clone().unwrap_or_default(),
+            request.value.clone().unwrap_or_default()
+        )
+        .into_bytes()
+    }
+
+    fn deserialize_request(&self, bytes: &[u8]) -> Result<Request, String> {
+        let request_str = String::from_utf8_lossy(bytes).to_string();
+        let parts: Vec<&str> = request_str.split_whitespace().collect();
+        if parts.len() < 2 {
+            Err("Invalid request".to_string())
+        } else {
+            Ok(Request {
+                command: parts[0].to_string(),
+                key: Some(parts[1].to_string()),
+                value: if parts.len() > 2 {
+                    Some(parts[2].to_string())
+                } else {
+                    None
+                },
+            })
+        }
+    }
+
+    fn serialize_response(&self, response: &Response) -> Vec<u8> {
+        format!("{}\n", response.message).into_bytes()
+    }
+
+    #[cfg(test)]
+    fn deserialize_response(&self, bytes: &[u8]) -> Result<Response, String> {
+        let response_str = String::from_utf8_lossy(bytes).to_string();
+        Ok(Response {
+            status: "OK".to_string(),
+            message: response_str,
+        })
+    }
+}
