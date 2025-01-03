cffdd7fca370b1d40699cd6019698d32a06bc05b
Safak Turkeli <sfktrkl@gmail.com>
2025-01-04 01:40:19 +0300

    Create multiple servers

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,36 @@
 use key_value_store::server::Server;
 
-const SERVER_ADDRESS: &str = "localhost:5000";
-const NODE_ID: u64 = 1; // Unique ID for this node
-const PEERS: &[u64] = &[2, 3]; // IDs of other nodes in the cluster
+const PEERS: &[(&str, u64)] = &[
+    ("localhost:5001", 1),
+    ("localhost:5002", 2),
+    ("localhost:5003", 3),
+];
 
 #[tokio::main]
 async fn main() {
-    let mut server = Server::new(SERVER_ADDRESS, PEERS.to_vec(), NODE_ID);
-    server.run().await;
+    let mut servers = Vec::new();
+
+    for &(address, id) in PEERS {
+        let peers: Vec<u64> = PEERS
+            .iter()
+            .filter(|&&(_, peer_id)| peer_id != id)
+            .map(|&(_, peer_id)| peer_id)
+            .collect();
+
+        let server = Server::new(address, peers, id);
+        servers.push(server);
+    }
+
+    let mut server_handles = Vec::new();
+    for mut server in servers {
+        server_handles.push(tokio::spawn(async move {
+            server.run().await;
+        }));
+    }
+
+    for handle in server_handles {
+        if let Err(e) = handle.await {
+            eprintln!("Server task encountered an error: {:?}", e);
+        }
+    }
 }
